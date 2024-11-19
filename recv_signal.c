ac10371bc29ea4e531bf6e1e612f4c44f46300c8
Christopher Myers <myersc@msoe.edu>
2024-11-18 20:19:14 -0600

    Created recv_signal.c and send_signal.c

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,36 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Christopher Myers
  * 
  * Brief summary of program:
+        Runs an infinite loop.
+        When it recieves a SIGUSR1 signal it prints out the sival_int
+        recieved from the signal and continues running.
+
  */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handle_usr1(int sig, siginfo_t *info, void *context);
+
+ int main(void) {
+
+    struct sigaction new_action;
+    new_action.sa_flags = SA_SIGINFO;
+    new_action.sa_sigaction = handle_usr1;
+
+    sigaction(SIGUSR1, &new_action, NULL);
+
+    // Infinite loop
+    while(1) {
+        sleep(1);
+    }
+
+    return 0;
+ }
+
+void handle_usr1(int sig, siginfo_t *info, void *context) {
+    printf("%d\n", info->si_value.sival_int);
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,41 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Christopher Myers
  * 
  * Brief summary of program:
+        Takes one command line argument of a pid value.
+        Then sends a SIGUSR1 signal with a random int value to the process.
  */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+
+    int pid;
+    if(argc == 1) { // If no argument check for this process
+        printf("Please enter a pid in the command argument\n");
+        return -1;
+    } else { // Parse the pid argument
+        pid = strtol(argv[1], NULL, 10);
+    }
+
+    if(kill(pid, 0) == -1) {
+        if(errno == ESRCH) {
+            printf("This process does not exist\n");
+        }
+        return -1;
+    } else {
+        srand(time(NULL));
+        const union sigval sendValue = {.sival_int = rand()};
+        printf("Sent Value: %d\n", sendValue.sival_int);
+
+        sigqueue(pid, SIGUSR1, sendValue);
+    }
+
+    return 0;
+}
